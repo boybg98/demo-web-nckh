8c02c024a3a16333272a09ceb5a532f192771ebe
Duc Anh 15t9 <duongducanh1509@gmail.com>
2025-06-14 15:35:21 +0700

    update code

--- main.js
+++ main.js
@@ -64,7 +64,6 @@ function speakWord(word) {
   utterance.lang = 'en-US';
   speechSynthesis.speak(utterance);
 }
-
 function showQuizButton(section, percent) {
   let quizButton = document.getElementById(`quiz-btn-${section}`);
 
@@ -91,13 +90,48 @@ function showQuizButton(section, percent) {
 }
 
 function startQuiz(section) {
+  localStorage.setItem('coutdown', 300);
+  localStorage.setItem('quizSection', section);
   window.location.href = "test1.html";
 }
-  var date = new Date();
-document.getElementById("d").innerHTML = date.toLocaleString();
-function dateTest(){
-  var day = date.getDate();
-  var hour = date.getTime();
-  document.getElementById("d").innerHTML = 'Ngày hôm nay là: ' + day;
-  document.getElementById("d").innerHTML = 'Giờ hiện tại là: ' + hour;
+function updateClock() {
+  const now = new Date();
+  const dateString = now.toLocaleString();
+  const clockElement = document.getElementById("d");
+  if (clockElement) {
+    clockElement.textContent = dateString;
+  }
 }
+setInterval(updateClock, 1000); 
+updateClock();
+let h = null, m = null, s = null;
+let timeout = null;
+function start() {
+    if (h === null) {
+        h = parseInt(document.getElementById('h_val').value);
+        m = parseInt(document.getElementById('m_val').value);
+        s = parseInt(document.getElementById('s_val').value);
+    }
+    if (s === -1) {
+        m -= 1;
+        s = 59;
+    }
+    if (m === -1) {
+        h -= 1;
+        m = 59;
+    }
+    if (h === -1) {
+        clearTimeout(timeout);
+        alert('⏰ Hết giờ làm bài!');
+        alert('Xin vui lòng chờ kết quả !');
+        return;
+    }
+    document.getElementById('h').innerText = String(h);
+    document.getElementById('m').innerText = String(m);
+    document.getElementById('s').innerText = String(s);
+    timeout = setTimeout(() => {
+        s-- ;
+        start();
+    }, 1000);
+}
+window.onload = start;
